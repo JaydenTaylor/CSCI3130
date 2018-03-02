e7d6b1ebe7a7a0347582b1bd870eeed534a6a7db
an956609 <an956609@dal.ca>
2018-03-02 13:22:56 -0400

    Jayden, Andre: Added firebase login authentication

--- code/app/src/main/java/ca/dal/cs/softeng/LoginActivity.java
+++ code/app/src/main/java/ca/dal/cs/softeng/LoginActivity.java
@@ -10,23 +10,52 @@ import static ca.dal.cs.softeng.common.Accounts.*;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private FirebaseAuth dbAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        init();
+        dbAuth.getInstance();
+    }
+    
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser account = dbAuth.getCurrentUser();
+        if(account != null) {
+            Intent calendar = new Intent(this, MainActivity.class);
+            startActivity(calendar);
+        }
     }
 
     public void signIn(View view) {
         String user = ((EditText)findViewById(R.id.email_sign_in)).getText().toString();
         String password = ((EditText)findViewById(R.id.password_sign_in)).getText().toString();
 
-        if(checkPassword(user, password)) {
-            Intent calendar = new Intent(this, MainActivity.class);
-            startActivity(calendar);
-        } else {
-            ((TextView)findViewById(R.id.error_sign_in)).setText(" Invalid username or password ");
-        }
+        dbAuth.signInWithEmailAndPassword(user, password)
+            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithEmail:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        if(user != null) {
+                            Intent calendar = new Intent(this, MainActivity.class);
+                            startActivity(calendar);
+                        }
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail:failure", task.getException());
+                        Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+                        updateUI(null);
+                    }
+
+                // ...
+                }
+            });
     }
 
     public void signUp(View view) {
